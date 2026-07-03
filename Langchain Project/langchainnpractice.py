fa2f56521e658f536d4c3d4e4335740531b3c7d3
Sohel Khan <sohelkhan123@gmail.com>
2026-07-03 22:59:29 +0530

    First Ollama Project

--- Langchain Project/langchainnpractice.py
+++ Langchain Project/langchainnpractice.py
@@ -1,5 +1,34 @@
 
-from langchain_ollama import OllamaLLM
+#from langchain_ollama import ChatOllama
+
+import os
+
+from httpcore import stream
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+os.environ["GOOGLE_API_KEY"]="AQ.Ab8RN6LD6VkqoKM7GpQVUY7fq-KSF4vbOwh8NgbBEAMI0iUx3A"
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+prompt_template = PromptTemplate.from_template(
+    """Explain {topic} to {audience} with these requirements:
+- Use {tone} tone
+- Give one real-life analogy to explain the concept
+- Keep the response within {limit} words"""
+)
+
+final_prompt=prompt_template.format(
+    topic="SQL Fundamentals",
+    audience="advance",
+    tone="harsh",
+    limit="300",
+    #stream=True
+)
+
+response = llm.invoke(final_prompt)
+print(response)
+print(response.content)
 
 
 
